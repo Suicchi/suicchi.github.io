62910db1b5f2a2e388c95415fc48d91f399a9db2
MD. Reazul Islam Bhuiyan <reazulislam@acm.org>
2018-12-19 19:31:11 +0600

    Updates

---
+++ precache-manifest.0c8095754011bd9630b524afb10eb30f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae090dddab09b9e7e82fa7c993ca5498",
+    "url": "/static/media/mesquare.ae090ddd.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb16584fb5496a3ffb9b",
+    "url": "/static/js/main.eb16584f.chunk.js"
+  },
+  {
+    "revision": "5b265051865492349dc6",
+    "url": "/static/js/1.5b265051.chunk.js"
+  },
+  {
+    "revision": "eb16584fb5496a3ffb9b",
+    "url": "/static/css/main.e976e8f3.chunk.css"
+  },
+  {
+    "revision": "2b00794b735b40e3292e220a0c499f19",
+    "url": "/index.html"
+  }
+];
